6539917baa1b35fd286a6a70080465c067367e6f
NawelB <nawel.berqouch@gmail.com>
2020-06-30 00:49:28 +0200

    update pom working

--- microservice-evenement/src/main/java/fr/isika/microservice/evenement/service/EventService.java
+++ microservice-evenement/src/main/java/fr/isika/microservice/evenement/service/EventService.java
@@ -1,17 +1,7 @@
 package fr.isika.microservice.evenement.service;
 
-import java.net.URI;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,19 +10,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
 
-import fr.isika.microservice.evenement.dao.EventRepository;
 import fr.isika.microservice.evenement.model.Event;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -84,7 +70,6 @@ public class EventService {
 				.log("********hey CREATE ************");	
 	}
 	
-	
 	@PutMapping ("private/admin/event")
 	public Mono<Event> updateEvent(@RequestBody Event event){
 		return client.put().uri("event-api/private/role-admin/updateEvent")
@@ -94,7 +79,6 @@ public class EventService {
 				.bodyToMono(Event.class)
 				.log("**Hey   UPDATE***********");
 	}
-	
 	
 	@RequestMapping(value = "public/event", params = "country" )
 	public Flux<Event>getEventByCountry(@RequestParam String country){
@@ -133,7 +117,6 @@ public class EventService {
 				.retrieve()
 				.bodyToMono(Event.class)
 				.log("**Hey   DELETE***********" + _id);
-		
 	}
 	
 	
@@ -172,16 +155,6 @@ public class EventService {
 //				.bodyToMono(Event.class)
 //				.log("********hey IIIIDDDDD************");	
 //	}
-	
-	
-	
-
-	
-
-	
-	
-	
-	
 	
 	
 	
